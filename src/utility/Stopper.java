364aff2a6531f1efe7b8849e3878544afce3a53e
leonleeldc <dingcheng@gmail.com>
2013-08-29 13:48:42 -0500

    file changes

--- src/utility/Stopper.java
+++ src/utility/Stopper.java
@@ -1,6 +1,7 @@
 package utility;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
@@ -10,10 +11,12 @@ public class Stopper {
 	private HashSet<String> stopwords = new HashSet<String>();
 	
 	public Stopper() throws IOException {
-		URL url = getClass().getResource("stopwords.txt");
+		//URL url = getClass().getResource("stopwords.txt");
+		File file = new File("data/stopwords.txt");
 		//System.out.println(url.getPath());
 		@SuppressWarnings("resource")
-		BufferedReader reader = new BufferedReader(new FileReader(url.getPath()));
+		//BufferedReader reader = new BufferedReader(new FileReader(url.getPath()));
+		BufferedReader reader = new BufferedReader(new FileReader(file));
 		String line;
 		while ( (line = reader.readLine()) != null) {
 			stopwords.add(line.trim());
