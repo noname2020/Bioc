f32d3c978d7d4c8f39c042cb32f24d327be55f25
m048100 <m048100@192.168.0.102>
2013-08-30 00:28:01 -0500

    query

--- src/CallIndriQuery.java
+++ src/CallIndriQuery.java
@@ -53,7 +53,8 @@ public class CallIndriQuery {
 		// TODO Auto-generated method stub
 		String pmid = args[0].substring(args[0].lastIndexOf("/")+1,args[0].indexOf("param")-1);
 		Runtime runtime = Runtime.getRuntime();
-		String makeCom = "IndriRunQuery "+args[0]+" > data/results/"+pmid+".result"; 
+		//String makeCom = "IndriRunQuery "+args[0]+" > data/results/"+pmid+".result";
+		String makeCom = "IndriRunQuery "+args[0];
 		System.out.println(makeCom);
 		Runtime rt = Runtime.getRuntime();
 		Process pr = rt.exec(makeCom);
